4f85ba6b15ce357ed119464cc35bef81fef1cdcc
salver <salver@192.168.1.100>
2016-11-28 09:10:02 +0800

    第三次上传

---
+++ src/evolution/JTest.java
@@ -0,0 +1,20 @@
+package evolution;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testEvolution() {
+		Evolution test = new Evolution(100);
+		//fail("Not yet implemented");
+	}
+
+}
--- src/evolution/Map.java
+++ src/evolution/Map.java
@@ -1,12 +1,21 @@
 package evolution;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Map extends JPanel implements Runnable {
@@ -18,11 +27,20 @@ public class Map extends JPanel implements Runnable {
 	int mapsize;
 	int[][] map;
 	int[][] nextmap;
-	int move[][] = { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 0 }, { 1, 1 },
-			{ 0, 1 }, { -1, 1 }, { -1, 0 } };
+	int life = 1;
+	int flag = 0;
+
+	int move[][] = { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }, { 0, 1 }, { -1, 1 }, { -1, 0 } };
 	Life life1;
+	JButton addBlock = new JButton("添加山脉");
+	JButton pause = new JButton("暂停");
+	JButton start = new JButton("开始");
+	
+	JPanel buttons = new JPanel();
+	
 
 	public Map(int size) {
+
 		this.mapsize = size;
 		map = new int[mapsize + 2][mapsize + 2];
 		nextmap = new int[mapsize + 2][mapsize + 2];
@@ -30,6 +48,16 @@ public class Map extends JPanel implements Runnable {
 		this.addKeyListener(new Key());
 		life1 = new Life(mapsize / 2, mapsize / 2);
 		map[life1.getX()][life1.getY()] = -1;
+		addBlock.addActionListener(new MountainButton());
+		pause.addActionListener(new PauseButton());
+		start.addActionListener(new StartButton());
+		buttons.setLayout(new FlowLayout());
+		buttons.add(addBlock);
+		buttons.add(pause);
+		buttons.add(start);
+		this.add(buttons,BorderLayout.SOUTH);
+		//contentpane.add(buttons, BorderLayout.SOUTH);
+		// addBlock.
 	}
 
 	public boolean die(int x, int y) {
@@ -38,8 +66,7 @@ public class Map extends JPanel implements Runnable {
 		for (i = 0; i < 8; i++) {
 			// map[i][j]==1 means there is a life alive
 			// map[i][j]==0 means the life is dead
-			if (map[x + move[i][0]][y + move[i][1]] == 1
-					|| map[x + move[i][0]][y + move[i][1]] == -1) {
+			if (map[x + move[i][0]][y + move[i][1]] == 1 || map[x + move[i][0]][y + move[i][1]] == -1) {
 				s++;
 			}
 		}
@@ -57,8 +84,7 @@ public class Map extends JPanel implements Runnable {
 		int i, s;
 		s = 0;
 		for (i = 0; i < 8; i++) {
-			if (map[x + move[i][0]][y + move[i][1]] == 1
-					|| map[x + move[i][0]][y + move[i][1]] == -1) {
+			if (map[x + move[i][0]][y + move[i][1]] == 1 || map[x + move[i][0]][y + move[i][1]] == -1) {
 				s++;
 			}
 		}
@@ -95,47 +121,62 @@ public class Map extends JPanel implements Runnable {
 	@Override
 	public void run() {
 		while (true) {
-			repaint();
-			refresh2();
-			for (int i = 1; i <= mapsize; i++) {
-				for (int j = 1; j <= mapsize; j++) {
-					if (map[i][j] == 0 || map[i][j] == -1) {
-						if (born(i, j)) {
-							setNextMap(i, j, 1);
+			
+			if(flag ==1){
+				
+			}else{
+				repaint();
+				refresh2();
+				for (int i = 1; i <= mapsize; i++) {
+					for (int j = 1; j <= mapsize; j++) {
+						if (map[i][j] == 0 || map[i][j] == -1) {
+							if (born(i, j)) {
+								setNextMap(i, j, 1);
+							}
 						}
-					}
-					if (map[i][j] == 1) {
-						if (die(i, j)) {
-							setNextMap(i, j, 0);
+						if (map[i][j] == 1) {
+							if (die(i, j)) {
+								setNextMap(i, j, 0);
+							}
 						}
 					}
 				}
+				if (motionless())
+					continue;
+				refresh1();
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-			if (motionless())
-				continue;
-			refresh1();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			
 		}
 		// System.out.println("The map is stable");
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		g.setColor(Color.black);
+		for (int i = 1; i <= mapsize + 1; i++) {
+			g.drawLine(10 + 10, 10 * i+50, 10 * mapsize + 20, 10 * i+50);
+			g.drawLine(10 * i + 10, 10+50, 10 * i + 10, 10 * mapsize + 10+50);
+		}
+
 		for (int i = 1; i <= mapsize; i++) {
 			for (int j = 1; j <= mapsize; j++) {
 				if (map[i][j] == 1) {
 					g.setColor(Color.BLACK);
-					g.fillRect(j * 6, i * 6, 6, 6);
+					// g.fillArc(arg0, arg1, arg2, arg3, arg4, arg5);
+					g.fillRect(j * 10 + 10, i * 10+50, 10, 10);
 				} else if (map[i][j] == -1) {
 					g.setColor(Color.orange);
-					g.fillRect(j * 6, i * 6, 6, 6);
-				} else {
-					//g.setColor(Color.BLACK);
-					//g.drawRect(i * 6, j * 6, 6, 6);
+					g.fillRect(j * 10 + 10, i * 10+50, 10, 10);
+				} else if (map[i][j] == 2) {
+					g.setColor(Color.RED);
+					g.fillRect(j * 10 + 10, i * 10+50, 10, 10);
+				} else {// g.setColor(Color.BLACK);
+						// g.drawRect(i * 6, j * 6, 6, 6);}
 				}
 			}
 		}
@@ -148,14 +189,47 @@ public class Map extends JPanel implements Runnable {
 	class Mouse extends MouseAdapter {
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			int j = (e.getX()) / 6;
-			int i = (e.getY()) / 6;
-			setNextMap(i, j, 1);
+			int j = (e.getX()) / 10 - 1;
+			int i = (e.getY()) / 10 -5;
+			if (map[i][j] == 2)
+				return;
+			setNextMap(i, j, life);
 			refresh1();
 			repaint();
 		}
+
 	}
 
+	class MountainButton implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			if (life == 1)
+				life = 2;
+			else if (life == 2)
+				life = 1;
+		}
+	}
+
+	class PauseButton implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			flag = 1;
+		}
+		
+	}
+	class StartButton implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			flag = 0;
+		}
+		
+	}
 	class Key extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
